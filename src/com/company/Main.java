6f09730195f778e28a6a0018a0f5a887b027b4c7
nathan-jayanthan <nathan.jayanthan@yahoo.com>
2020-06-29 21:41:05 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //first is starting,second is range, third is increment
+        for(int counter =1; counter <=10; counter+=3){
+            System.out.println(counter);
+        }
+
+    }
+}
